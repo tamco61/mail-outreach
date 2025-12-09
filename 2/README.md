f0cad22c41808008326c04a07c280c5f2d570e8c
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-12-10 00:09:21 +0300

    update 2

--- 2/README.md
+++ 2/README.md
@@ -3,10 +3,17 @@
 pip install -r requirement.txt
 ```  
 
-# Запуск
+# Запуск в случае, когда id чата известно
 ```(bash)
 python .\main.py <token-file-path> <message-file-path> <chat-id>
 ```  
 **token-file-path** файл с токеном бота  
 **message-file-path** файл с текстом сообщения  
 **chat-id** id чата куда нужно отправить сообщение
+
+# Запуск в случае, когда id чата неизвестно
+```(bash)
+python .\main.py <token-file-path> <message-file-path>
+```  
+**token-file-path** файл с токеном бота  
+**message-file-path** файл с текстом сообщения
--- 2/main.py
+++ 2/main.py
@@ -8,6 +8,25 @@ def get_text(filename):
     return text
 
 
+def get_chats(token):
+    url = f"https://api.telegram.org/bot{token}/getUpdates"
+    response = requests.get(url)
+    result = response.json()["result"]
+
+    chats = {}
+    for update in result:
+        chat = update["message"]["chat"]
+        chat_id = chat["id"]
+        if chat_id not in chats:
+            if "title" in chat:
+                title = chat["title"]
+            else:
+                title = chat["username"]
+            chats[chat_id] = title
+
+    return chats
+
+
 def send_message(token, message, chat_id):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
 
@@ -21,12 +40,31 @@ def send_message(token, message, chat_id):
 
 
 def main():
-    if len(sys.argv) == 4:
-        token_filename, message_filename, chat_id = sys.argv[1:]
-        chat_id = int(chat_id)
+    if len(sys.argv) == 3:
+        token_filename, message_filename = sys.argv[1:]
+
         token = get_text(token_filename)
         message = get_text(message_filename)
-        send_message(token, message, chat_id)
+        chats = get_chats(token)
+
+        print("Доступные чаты:")
+        print("id\t\ttitle")
+        for chat_id, title in chats.items():
+            print(chat_id, title, sep="\t")
+        print("Выберите доступный чат (в качестве выбора отправьте наберите его id):")
+        chat_id = int(input())
+
+    elif len(sys.argv) == 4:
+        token_filename, message_filename, chat_id = sys.argv[1:]
+
+        token = get_text(token_filename)
+        message = get_text(message_filename)
+        chat_id = int(chat_id)
+    else:
+        print("неверный формат ввода")
+        sys.exit(1)
+
+    send_message(token, message, chat_id)
 
 
 if __name__ == "__main__":
